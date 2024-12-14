5130b73db5f2e264bdca06821a3271e463dd2add
Antoine Gavira <antoine.gavira-bottari@epitech.eu>
2024-12-14 14:02:28 +0100

    feat(landing): new landing layout and style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,17 @@
+import SideBar from '../components/SideBar';
+import TopBar from '../components/TopBar';
 import styles from '../style/Landing.module.css';
-import Link from 'next/link';
 
 function Home() {
     return (
-      <div className={styles.Landing}>
-      <header className={styles.LandingHeader}>
-          <h1>Welcome to StepForward</h1>
-          <nav>
-              <ul>
-                  <li><Link href="/exercise">Exercise</Link></li>   { /* accessible if not logged in */ }
-                  <li><Link href="/calendar">Calendar</Link></li>   { /* accessible if logged in */ }
-                  <li><Link href="/login">Login</Link></li>         { /* not accessible if logged in */ }
-                  <li><Link href="/register">Register</Link></li>   { /* not accessible if logged in */ }
-              </ul>
-          </nav>
-      </header>
-  </div>
+        <div className={styles.Landing}>
+            <TopBar />
+            <SideBar />
+            <main className={styles.main}>
+                <h1>StepForward Landing Page</h1>
+                {/* Add other content here */}
+            </main>
+        </div>
     );
 }
 
